e08d03fbfdc76d0c5aff036cba4b8e306f254b66
Shayla White <shayladwhite1@gmail.com>
2020-04-05 20:48:39 +0000

    Automatically backed up by Learn

--- db/migrate/20200405203152_create_users.rb
+++ db/migrate/20200405203152_create_users.rb
@@ -1,5 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
-    
-  end
+   create_table :users do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
 end
