9328aa182b7fc1d19ef261af3ce1933e77f225cb
Shayla White <shayladwhite1@gmail.com>
2020-04-05 20:51:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200405203152) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "cook_time"
+  end
 
 end
