4df071b1adb230a15305c9857b58deda7e951e0f
Shayla White <shayladwhite1@gmail.com>
2020-04-05 21:44:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
   
-    get '/recipes/:id' do
+    get '/recipes/:id/edit' do
   @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
