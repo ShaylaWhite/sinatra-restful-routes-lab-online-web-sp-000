fe81b5721bf106a00168a149c5b68732aa3acb0b
Shayla White <shayladwhite1@gmail.com>
2020-04-05 21:54:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,23 +31,24 @@ class ApplicationController < Sinatra::Base
   end
   
    get '/recipes/:id' do
-  @recipe = Recipe.find_by_id(params[:id])
+       @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
   
     get '/recipes/:id/edit' do
-  @recipe = Recipe.find_by_id(params[:id])
+       @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
   
   patch '/recipes/:id' do 
-  @recipe = Recipe.find_by_id(params[:id])
-  @recipe.name = params[:name]
-  @recipe.ingredients = params[:ingredients]
-  @recipe.cook_time = params[:cook_time]
-
-  @recipe.save
+        @recipe = Recipe.find_by_id(params[:id])
+        @recipe.name = params[:name]
+        @recipe.ingredients = params[:ingredients]
+        @recipe.cook_time = params[:cook_time]
+      
+        @recipe.save
   redirect to "/recipes/#{@recipe.id}"
+  
 end
   
 end
